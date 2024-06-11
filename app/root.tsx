1632434e3b1d43be4b2ba47ea35211082485b572
Shannon Oram <shannon.oram@gmail.com>
2024-06-11 12:05:07 +1000

    Add tailwind reset

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { type LinksFunction } from '@remix-run/cloudflare'
 import {
   Links,
   Meta,
@@ -5,6 +6,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react'
+import resetStyles from '~/styles/reset.css?url'
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: resetStyles },
+]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
